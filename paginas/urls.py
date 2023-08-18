7110925bed6fb2ae99faf52734b941efbdfb6dca
andersonlimaz <andersonlimaz@outlook.com>
2023-08-18 10:34:41 -0300

    commit projeto 18/08/2023

---
+++ paginas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import IndexView
+
+urlpatterns = [ 
+    # Sempre que for criar path caminho 
+    #path('endereço', MinhaView.as_view(), name='Nome-da-url'),
+    path('inicio/', IndexView.as_view(), name='inicio'),
+]
