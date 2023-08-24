8225fb245a2f94a8fd7d4c9da9096efdd85f878c
andersonlimaz <andersonlimaz@outlook.com>
2023-08-24 10:00:10 -0300

    commit pagina index, estou desenvolvento pagina de pagamento

--- paginas/views.py
+++ paginas/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class PaginaInicial(TemplateView):
     template_name = "index.html"
+
+class Paginapg(TemplateView):
+    template_name ="pagamento.html"
